ddedba15e9bb01cce8834893d3c6e8e06a7ac3f5
Gimna Katugampala <gimnakatugampala1@gmail.com>
2021-04-19 08:47:25 +0530

    template

---
+++ resources/views/basic/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="backdrop-filter backdrop-opacity-25 backdrop-contrast-75">
+    <img src="{{asset('img/showcase.jpg')}}" class="backdrop-filter backdrop-opacity-25">
+</div>
+
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,27 @@
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 
-<body>
+<body class="bg-gray-200">
+    <!-- Navbar -->
+
+    <nav class="p-6 bg-white flex justify-between">
+        <ul class="flex items-left">
+            <li><a href="{{route('home')}}" class="p-3 text-2xl">Techblog</a></li>
+        </ul>
+
+        <ul class="flex items-center">
+            <li><a href="{{route('home')}}" class="p-3">Home</a></li>
+            <li><a href="" class="p-3">About</a></li>
+            <li><a href="" class="p-3">Contact Us</a></li>
+            <li><a href="" class="p-3">Services</a></li>
+            <li><a href="" class="p-3">Blog</a></li>
+            <li><a href="" class="p-3">Login</a></li>
+            <li><a href="" class="p-3">Register</a></li>
+            <li><a href="" class="p-3">Logout</a></li>
+        </ul>
+
+    </nav>
+
 
     @yield('content')
 
